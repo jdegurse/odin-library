83a4dad550e440247e57c4feaf9861cc8b972435
James DeGurse <jamesdegurse@gmail.com>
2021-02-06 17:33:06 -0500

    Create Book() and addBookToLibrary()
    Created the Book() constructor, and the addBookToLibrary() function so
    that Book objects can be added to the library array.

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,14 @@
 // Library Array
-let library = []
+let library = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    library.push(new Book(title, author, pages, read))
+}
 
