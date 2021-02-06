024d7a86762a909e74e46e9b28cd6e10d71d9ce5
James DeGurse <jamesdegurse@gmail.com>
2021-02-06 18:41:05 -0500

    [feat] Implement "add book" functionality
    Coded the submitClick function which will query the HTML form for book
    information and send it to the addBookToLibrary() function. This will
    add books to the library array.
    
    Also added the invalidEntryWarning() function, which will be a
    placeholder until something better can be implemented in the future.
    
    Needed to change the HTML form from a hard coded <form> to straight
    input boxes with no <form> tag. Was too difficult to handle the
    onsubmit event without reloading the page. Now submitClick() will just
    clear fields (need to refactor into its own function).

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 // Library Array
 let library = [];
 
+////////    BOOK CREATION    ////////
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -12,3 +13,27 @@ function addBookToLibrary(title, author, pages, read) {
     library.push(new Book(title, author, pages, read))
 }
 
+function submitClick() {
+    let title = document.getElementById('add-title');
+    let author = document.getElementById('add-author');
+    let pages = document.getElementById('add-pages');
+    let read = document.getElementById('add-read');
+    if (title.value === '' || author.value === '' || pages.value === '') {
+        invalidEntryWarning();
+        return;
+    }
+    addBookToLibrary(
+        title.value,
+        author.value,
+        pages.value,
+        read.checked
+    );
+    title.value = author.value = pages.value = '';
+    read.checked = false;
+}
+
+function invalidEntryWarning() {
+    alert('Invalid Entry')
+}
+
+document.getElementById('add-submit').addEventListener('click', submitClick);
