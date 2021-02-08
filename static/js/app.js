a26024a1d03c073f9445c984e26c0d60c303e02f
James DeGurse <jamesdegurse@gmail.com>
2021-02-07 20:27:53 -0500

    Wrap "add book" in accordion
    The "add book" area has been wrapped in an accordion to keep the app
    clean.

--- static/js/app.js
+++ static/js/app.js
@@ -184,6 +184,25 @@ function reassignDataIndex() {
 
 
 
+////////    ACCORDION    ////////
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+
+
 ////////    ON LOAD    ////////
 createTable()
 
